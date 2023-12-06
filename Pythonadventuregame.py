16dd0fcc0ec8dc3439436b8f36cb9344dab4e5a7
karolvsrex <152913477+karolvsrex@users.noreply.github.com>
2023-12-06 16:35:52 -0500

    Update Pythonadventuregame.py

--- Pythonadventuregame.py
+++ Pythonadventuregame.py
@@ -1,17 +1,36 @@
-name = input(" Enter your Character's name: ")
-print("Welcome"  ,name, "to the World of Vardheim")
+import streamlit as st
 
-answer = input("You have the option to go left or right, which way do you travel? ").lower()
+def main():
+    st.title("Welcome to the World of Vardheim")
 
-if answer == "left":
-    answer = input('You have entered Elwynn Forest. Do you want to take down the Defias Brotherhood?')
+    # Character name input
+    name = st.text_input("Enter your Character's name: ")
+    if name:
+        st.write("Welcome", name, "to the World of Vardheim")
 
-    if answer == "yes":
-        print("Congratulations, you are victorious!")
+        # Choose direction
+        direction = st.radio("You have the option to go left or right, which way do you travel?", ('Left', 'Right'))
 
-    elif answer == "no":
-        print("Congratulations, you accidentally stumble into BlackRock Mountain and were consumed by Ragnaros the Firelord. ")
-    else: 
-        print("You've made a grave miscalculation. The Defias Brotherhood sought you out and ended you")
-elif answer == "right":
-    print("You make a right turn from Elwynn Forest and make your way into the Redridge Mountains")
+        if direction == 'Left':
+            # Left path options
+            left_path = st.button('Enter Elwynn Forest')
+
+            if left_path:
+                # Encounter in Elwynn Forest
+                st.write("You have entered Elwynn Forest.")
+                defeat_defias = st.radio("Do you want to take down the Defias Brotherhood?", ('Yes', 'No'))
+
+                if defeat_defias == 'Yes':
+                    st.write("Congratulations, you are victorious!")
+                elif defeat_defias == 'No':
+                    st.write("Congratulations, you accidentally stumble into BlackRock Mountain and were consumed by Ragnaros the Firelord.")
+                else:
+                    st.write("You've made a grave miscalculation. The Defias Brotherhood sought you out and ended you")
+
+        elif direction == 'Right':
+            # Right path
+            if st.button('Travel right from Elwynn Forest'):
+                st.write("You make a right turn from Elwynn Forest and make your way into the Redridge Mountains")
+
+if __name__ == "__main__":
+    main()
